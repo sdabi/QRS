125722f7c568da4755e9c3acb39eb2b0772b7b28
ShlomiD <shlomodabi@gmail.com>
2022-08-06 11:51:58 +0300

    edtis in QRS model 1

--- embedded_QRS.py
+++ embedded_QRS.py
@@ -4,6 +4,7 @@ from pennylane.templates.embeddings import AngleEmbedding
 from pennylane.templates.layers import BasicEntanglerLayers
 from pennylane import numpy as np
 import time
+import itertools
 
 import math
 import defines
@@ -20,6 +21,21 @@ wires_list = item_wires + user_wires
 # weights =  np.random.random((1, n_wires))
 weights_users = np.zeros((1, n_item_wires), requires_grad=False)
 weights_all = np.zeros((1, n_wires), requires_grad=False)
+
+
+
+# this cricuit is used to plot the embeding quantum state only
+dev_embedded_ItemItem_embedding = qml.device('default.qubit', wires=n_wires)
+@qml.qnode(dev_embedded_ItemItem_embedding)
+def embedded_QRS_circ_embedding(embedded_params):
+    for wire in item_wires:
+        qml.Hadamard(wire)
+    AngleEmbedding(embedded_params, wires=item_wires, rotation='Z')
+    BasicEntanglerLayers(weights_users, wires=item_wires)
+    for wire in item_wires:
+        qml.Hadamard(wire)
+    return qml.probs(wires=item_wires)
+
 
 
 
@@ -54,17 +70,30 @@ class embedded_QRS_model1():
         self.user_embedded_vecs = user_embedded_vecs
         self.item_embedded_vecs = item_embedded_vecs
         self.normalize_embdded_vecotrs()
-        visualiser.plot_embedded_vecs(self.user_embedded_vecs)
+        # plotting the embedded quantum states:
+        embedded_state = []
+        for user in range(defines._NUM_OF_USERS):
+            probs = embedded_QRS_circ_embedding(self.user_embedded_vecs[user])
+            embedded_state.append(probs)
+        visualiser.plot_embedded_vecs(embedded_state)
+        # visualiser.plot_embedded_vecs_3d(embedded_state)
 
         self.interacted_items_matrix = self.create_interacted_items_matrix()
         self.bad_interacted_items_matrix = self.create_bad_interacted_items_matrix()
         self.uninteracted_items_matrix = self.create_uninteracted_items_matrix()
         self.expected_probs_vecs = self.create_expected_probs_vecs()
 
+        self.avg_num_of_interactions = 0
+        for user in range(defines._NUM_OF_USERS):
+            self.avg_num_of_interactions += len(self.interacted_items_matrix[user])
+        self.avg_num_of_interactions /= defines._NUM_OF_USERS
+
+        print(self.avg_num_of_interactions)
         self.params = np.array(init_parms, requires_grad=True)
         self.train_steps = train_steps
         self.error_ver_weights = self.get_error_vec_weights()
         self.common_item_vec = self.calc_common_items_vecs()
+        self.error_vec_weights_for_uninter = self.get_error_vec_weights_for_uninteracted_itesm()
         self.total_cost = []
         self.error_per_user = []
         for i in range(defines._NUM_OF_USERS):
@@ -125,9 +154,7 @@ class embedded_QRS_model1():
         print("\n------- TRAINING EMBEDDED ITEM RECOMMENDATION SYS -------")
 
         for i in range(self.train_steps):
-            if i % 10 == 0:
-                print("training step:", i)
-                # print(self.params)
+            print("training step:", i)
             self.params = opt_item_item.step(lambda v: self.total_cost_embedded_QRS(v), self.params)
 
         print("--- embedding train took %s seconds ---" % math.ceil(time.time() - start_time_train))
@@ -150,8 +177,11 @@ class embedded_QRS_model1():
             # calc the error for the user
             error_per_item = (expected_probs - probs)
 
-            # remove error on uninteracted items - punishing only on interacted ones
             uninteracted_items = self.uninteracted_items_matrix[user]
+            bad_interacted_items = self.bad_interacted_items_matrix[user]
+            interacted_items = self.interacted_items_matrix[user]
+
+            # remove error on uninteracted items - punishing only on interacted ones
             if len(uninteracted_items) != 0:
                 error_per_item._value[uninteracted_items] = 0 # this is an autoguard object - accessing to its values
 
@@ -160,31 +190,35 @@ class embedded_QRS_model1():
             # error_per_item = np.maximum(error_per_item_negative, error_per_item)
 
             # add punishment to bad interacted items
-            bad_interacted_items = self.bad_interacted_items_matrix[user]
+
             error_per_item._value[bad_interacted_items] = error_per_item._value[bad_interacted_items]*10
+            error_per_item._value[interacted_items] = error_per_item._value[interacted_items]*2
             error_per_item = error_per_item**2
 
-            # error_per_item = error_per_item * self.error_ver_weights
-            # error_per_item = error_per_item * self.common_item_vec
+            error_per_item = error_per_item * self.error_ver_weights
+            error_per_item = error_per_item * self.common_item_vec
 
-
-            cost_for_user = sum(error_per_item)
+            cost_for_user = sum(error_per_item)*(len(interacted_items)/self.avg_num_of_interactions)
             self.error_per_user[user].append(cost_for_user._value)
 
             total_cost += cost_for_user
 
             # DEBUG:
-            # if user == 0:
-            #     print("user:", user)
-            #     visualiser.print_colored_matrix(probs._value, [bad_interacted_items, interacted_items], is_vec=1,
-            #                                     all_positive=1, digits_after_point=2)
-            #     # visualiser.print_colored_matrix(self.error_ver_weights, [bad_interacted_items, interacted_items], is_vec=1,
-            #     #                                 all_positive=1, digits_after_point=2)
-            #     # visualiser.print_colored_matrix(self.common_item_vec, [bad_interacted_items, interacted_items], is_vec=1,
-            #     #                                 all_positive=1, digits_after_point=2)
-            #     # visualiser.print_colored_matrix(error_per_item._value, [bad_interacted_items, interacted_items], is_vec=1,
-            #     #                                 all_positive=1, digits_after_point=2)
-            #     print("cost_for_user", cost_for_user._value, "\n")
+            if user == 8:
+                print("user:", user)
+                visualiser.print_colored_matrix(expected_probs, [bad_interacted_items, interacted_items], is_vec=1,
+                                                all_positive=1, digits_after_point=2)
+                visualiser.print_colored_matrix(probs._value, [bad_interacted_items, interacted_items], is_vec=1,
+                                                all_positive=1, digits_after_point=2)
+                # visualiser.print_colored_matrix(self.error_ver_weights, [bad_interacted_items, interacted_items], is_vec=1,
+                #                                 all_positive=1, digits_after_point=2)
+                # visualiser.print_colored_matrix(self.error_vec_weights_for_uninter, [bad_interacted_items, interacted_items], is_vec=1,
+                #                                 all_positive=1, digits_after_point=2)
+                # visualiser.print_colored_matrix(self.common_item_vec, [bad_interacted_items, interacted_items], is_vec=1,
+                #                                 all_positive=1, digits_after_point=2)
+                visualiser.print_colored_matrix(error_per_item._value, [bad_interacted_items, interacted_items], is_vec=1,
+                                                all_positive=1, digits_after_point=2)
+                print("cost_for_user", cost_for_user._value, "\n")
 
         print("total_cost:", total_cost._value)
         print("------------------------------------","\n")
@@ -194,13 +228,17 @@ class embedded_QRS_model1():
     # input: list of vectors
     # output: list of vectors which all positive, and the sum of each vector is smaller than pi
     def normalize_embdded_vecotrs(self):
-        self.user_embedded_vecs -= self.user_embedded_vecs.min(axis=0)     # min in evey columns is 0
-        self.user_embedded_vecs /= (self.user_embedded_vecs.max()+0.0001)  # max in all data is 1
-        self.user_embedded_vecs *= (math.pi/defines._EMBEDDING_SIZE)       # sum of each row is up to pi
+        columns_mins = self.user_embedded_vecs.min(axis=0)
+        self.user_embedded_vecs -= columns_mins     # min in evey columns is 0
 
-        self.item_embedded_vecs -= self.item_embedded_vecs.min(axis=0)  # min in evey columns is 0
-        self.item_embedded_vecs /= (self.item_embedded_vecs.max() + 0.0001)  # max in all data is 1
-        self.item_embedded_vecs *= (math.pi / defines._EMBEDDING_SIZE)  # sum of each row is up to pi
+        global_max = self.user_embedded_vecs.max()+0.0001
+        self.user_embedded_vecs /= global_max  # max in all data is 1
+
+        self.user_embedded_vecs *= (2*math.pi/defines._EMBEDDING_SIZE)       # sum of each row is up to pi
+
+        # self.item_embedded_vecs -= self.item_embedded_vecs.min(axis=0)  # min in evey columns is 0
+        # self.item_embedded_vecs /= (self.item_embedded_vecs.max() + 0.0001)  # max in all data is 1
+        # self.item_embedded_vecs *= (math.pi / defines._EMBEDDING_SIZE)  # sum of each row is up to pi
 
     def get_recommendation(self, user, uninteracted_items, removed_movie):
         # get the probs vector for user
@@ -236,6 +274,10 @@ class embedded_QRS_model1():
         return self.params
 
 
+    # return vec - where:
+    #     items that consumed many times - will have smaller value
+    #     items that consumed less times - will have bigger value
+    # this protects popular items to get the prop to them
     def get_error_vec_weights(self):
         error_vec_weights = np.zeros(defines._NUM_OF_ITEMS)
         for user in range(defines._NUM_OF_USERS):
@@ -245,14 +287,36 @@ class embedded_QRS_model1():
         error_vec_weights = [1/i if i>0 else 0 for i in error_vec_weights]
         return error_vec_weights
 
+    # return vec - where:
+    #     items that consumed many times - will have smaller value
+    #     items that consumed less times - will have bigger value
+    # this protects popular items to get the prop to them
+    def get_error_vec_weights_for_uninteracted_itesm(self):
+        error_vec_weights = np.zeros(defines._NUM_OF_ITEMS)
+        for user in range(defines._NUM_OF_USERS):
+            interacted_items = np.where(self.R[user] == 1)[0]
+            for i in interacted_items:
+                error_vec_weights[i] += 1
+        max_val = max(error_vec_weights)
+        error_vec_weights = [max_val/i if i>0 else max_val for i in error_vec_weights]
+        return error_vec_weights
 
+
+
+    # calculating for every item - with how many items it comes on average
+    # e.g. item 'i' comes always with 3 other items
+    # normalizing it to 1
+    # items that come as part of a big group we will punish more - because it's expected probs is smaller to begin with
+    # logic here is that items that consumed with many other items - then it's a must item (like sequel)
+    # items that come as part of a small group - we will punish less - its expected will be larger
+    # logic here is the there isn't strong basis for this item - maybe user which watch random staff takes it
     def calc_common_items_vecs(self):
-        common_item_vec = np.zeros(defines._NUM_OF_ITEMS)
-        item_interacted_count = np.zeros(defines._NUM_OF_ITEMS)
+        common_item_vec = np.zeros((defines._NUM_OF_ITEMS), requires_grad=False)
+        item_interacted_count = np.zeros((defines._NUM_OF_ITEMS), requires_grad=False)
         for user in range(defines._NUM_OF_USERS):
             for item in range(defines._NUM_OF_ITEMS):
                 if self.R[user][item] == 1:
-                    item_interacted_count[item] += 1
+                    item_interacted_count[item] += 1 # number if interatcions marked for this item
                     common_item_vec[item] += np.count_nonzero(self.R[user] == 1) - 1 # adding the other interactions for user
 
         common_item_vec = [item/count if count > 0 else 0 for (item,count) in zip (common_item_vec, item_interacted_count) ] # calc average item interactions
