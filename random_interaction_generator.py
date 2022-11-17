ff34bc492c365f7b2c14a71fe35875088e8949be
ShlomiD <shlomodabi@gmail.com>
2022-11-17 08:33:34 +0200

    edit random data generator - so every usr will have same num of positive interactions

--- random_interaction_generator.py
+++ random_interaction_generator.py
@@ -6,46 +6,23 @@ import random
 
 class random_interactions_data_generator():
 
-    def __init__(self, num_of_interactions, num_of_bad_inters):
+    def __init__(self, num_of_interactions, num_of_inter_per_user, num_of_bad_inters):
 
         self.rating_df = pd.DataFrame([], columns=['userId', 'movieId', 'rating', 'timestamp'])
 
-        for inter_num in range(num_of_interactions):
-            inter_counts = np.zeros(defines._NUM_OF_ITEMS)
-            # picking a user to add interaction to
-            user_to_add_inter = random.randint(0, defines._NUM_OF_USERS-1)
-
-            # getting all his previous interactions
-            all_interacted_movies_for_user = self.rating_df.loc[self.rating_df.userId == user_to_add_inter, 'movieId'].values.tolist()
-
-            # searching for users with same interactions
-            for inter in all_interacted_movies_for_user:
-                users_with_same_inter = self.rating_df.loc[self.rating_df.movieId == inter, 'userId'].values.tolist()
-
-                # for each user with same interaction - get all his other interactions
-                for user_with_same_inter in users_with_same_inter:
-                    movies_user_with_same_inter_also_had = self.rating_df.loc[self.rating_df.userId == user_with_same_inter, 'movieId'].values.tolist()
-
-                    # and sum them into array (histograms)
-                    inter_counts[movies_user_with_same_inter_also_had] += 1
-
-            # removing from the histogram all the previous interaction for the user
-            inter_counts[all_interacted_movies_for_user] = -1
-
-            max_interacted_item = np.random.choice(np.where(inter_counts == inter_counts.max())[0], replace=False, size=1)[0]
-            # print("user",user_to_add_inter,"choose",max_interacted_item,"score",inter_counts.max())
-            inter_as_df = pd.DataFrame([[user_to_add_inter, max_interacted_item, 1, inter_num]], columns=['userId', 'movieId', 'rating', 'timestamp'])
-            self.rating_df = pd.concat([self.rating_df, inter_as_df])
-
-        # make sure that for every user there are at least 2 interactions
-        timestamp = num_of_interactions
-        for i in range(2):
-            for user_to_add_inter in range(defines._NUM_OF_USERS):
+        # TO ENABLE RANDOM NUM OF INTERACTION FOR USER:
+        # comment the following 2 for loops - and uncomment the 3'rd one + uncomment user_to_add_inter
+        for inter in range(num_of_inter_per_user):
+            for user in range(defines._NUM_OF_USERS):
+            # for inter_num in range(num_of_interactions):
+                inter_num = inter + user
                 inter_counts = np.zeros(defines._NUM_OF_ITEMS)
+                # picking a user to add interaction to
+                # user_to_add_inter = random.randint(0, defines._NUM_OF_USERS-1) # UNCOMMENT ME - when needed to be random
+                user_to_add_inter = user
 
                 # getting all his previous interactions
-                all_interacted_movies_for_user = self.rating_df.loc[
-                    self.rating_df.userId == user_to_add_inter, 'movieId'].values.tolist()
+                all_interacted_movies_for_user = self.rating_df.loc[self.rating_df.userId == user_to_add_inter, 'movieId'].values.tolist()
 
                 # searching for users with same interactions
                 for inter in all_interacted_movies_for_user:
@@ -53,8 +30,7 @@ class random_interactions_data_generator():
 
                     # for each user with same interaction - get all his other interactions
                     for user_with_same_inter in users_with_same_inter:
-                        movies_user_with_same_inter_also_had = self.rating_df.loc[
-                            self.rating_df.userId == user_with_same_inter, 'movieId'].values.tolist()
+                        movies_user_with_same_inter_also_had = self.rating_df.loc[self.rating_df.userId == user_with_same_inter, 'movieId'].values.tolist()
 
                         # and sum them into array (histograms)
                         inter_counts[movies_user_with_same_inter_also_had] += 1
@@ -62,13 +38,43 @@ class random_interactions_data_generator():
                 # removing from the histogram all the previous interaction for the user
                 inter_counts[all_interacted_movies_for_user] = -1
 
-                max_interacted_item = np.random.choice(np.where(inter_counts == inter_counts.max())[0], replace=False, size=1)[
-                    0]
+                max_interacted_item = np.random.choice(np.where(inter_counts == inter_counts.max())[0], replace=False, size=1)[0]
                 # print("user",user_to_add_inter,"choose",max_interacted_item,"score",inter_counts.max())
-                inter_as_df = pd.DataFrame([[user_to_add_inter, max_interacted_item, 1, timestamp]],
-                                           columns=['userId', 'movieId', 'rating', 'timestamp'])
+                inter_as_df = pd.DataFrame([[user_to_add_inter, max_interacted_item, 1, inter_num]], columns=['userId', 'movieId', 'rating', 'timestamp'])
                 self.rating_df = pd.concat([self.rating_df, inter_as_df])
-                timestamp +=1
+
+        # make sure that for every user there are at least 2 interactions
+        timestamp = inter_num
+        # for i in range(2):
+            # for user_to_add_inter in range(defines._NUM_OF_USERS):
+            #     inter_counts = np.zeros(defines._NUM_OF_ITEMS)
+            #
+            #     # getting all his previous interactions
+            #     all_interacted_movies_for_user = self.rating_df.loc[
+            #         self.rating_df.userId == user_to_add_inter, 'movieId'].values.tolist()
+            #
+            #     # searching for users with same interactions
+            #     for inter in all_interacted_movies_for_user:
+            #         users_with_same_inter = self.rating_df.loc[self.rating_df.movieId == inter, 'userId'].values.tolist()
+            #
+            #         # for each user with same interaction - get all his other interactions
+            #         for user_with_same_inter in users_with_same_inter:
+            #             movies_user_with_same_inter_also_had = self.rating_df.loc[
+            #                 self.rating_df.userId == user_with_same_inter, 'movieId'].values.tolist()
+            #
+            #             # and sum them into array (histograms)
+            #             inter_counts[movies_user_with_same_inter_also_had] += 1
+            #
+            #     # removing from the histogram all the previous interaction for the user
+            #     inter_counts[all_interacted_movies_for_user] = -1
+            #
+            #     max_interacted_item = np.random.choice(np.where(inter_counts == inter_counts.max())[0], replace=False, size=1)[
+            #         0]
+            #     # print("user",user_to_add_inter,"choose",max_interacted_item,"score",inter_counts.max())
+            #     inter_as_df = pd.DataFrame([[user_to_add_inter, max_interacted_item, 1, timestamp]],
+            #                                columns=['userId', 'movieId', 'rating', 'timestamp'])
+            #     self.rating_df = pd.concat([self.rating_df, inter_as_df])
+            #     timestamp +=1
 
         # add bad interactions - based on interaction which user loved/not loved
         for inter_num in range(num_of_bad_inters):
